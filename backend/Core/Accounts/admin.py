a9ef09c533ec32cec596fea513dbf2e917c84a92
AliZamani696 <azamani081@gmail.com>
2025-12-24 15:14:34 +0330

    create admin panel

--- backend/Core/Accounts/admin.py
+++ backend/Core/Accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'role', 'wallet_balance', 'is_premium', 'is_staff')
+
+    list_filter = ('role', 'is_staff', 'is_active')
+
+    list_editable = ('role', 'wallet_balance')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        #(_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Financial & Subscription'), {
+            'fields': ('wallet_balance', 'subscription_expires_at'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1','password2', 'role', 'is_staff', 'is_active'),
+        }),
+    )
+
+    ordering = ('-date_joined',)
+    search_fields = ('email',)
+
+    @admin.display(boolean=True, description='Premium')
+    def is_premium(self, obj):
+        return obj.is_premium
--- backend/Core/Accounts/models.py
+++ backend/Core/Accounts/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+         if not email:
+            raise ValueError(_("The Email must be set"))
+         email = self.normalize_email(email)
+         user = self.model(email=email, **extra_fields)
+         user.set_password(password)
+         user.save()
+         return user
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", 'ADMIN')
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN",_("Admin")
+        SUBSCRIBER = 'SUBSCRIBER', _('Subscriber')
+        REGULAR = 'REGULAR', _('Regular')
+
+    email = models.EmailField(_("email address"), unique=True)
+    role = models.CharField(max_length=20,choices=Role.choices,default=Role.REGULAR)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    wallet_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    subscription_expires_at = models.DateTimeField(null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    @property
+    def is_premium(self):
+        if self.role == self.Role.ADMIN:
+            return True
+        return (
+            self.role == self.Role.SUBSCRIBER and
+            self.subscription_expires_at and
+            self.subscription_expires_at > timezone.now()
+            )
+
+    def __str__(self):
+        return self.email
